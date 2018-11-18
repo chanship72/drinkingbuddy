f8bf63af1dfa12af2440f428ec5a91490188dec5
cspark <chanship@usc.edu>
2018-11-17 20:07:21 -0800

    add footage,imagelist

--- public/js/controller.js
+++ public/js/controller.js
@@ -4,5 +4,47 @@ drinkControllers.controller('drinkController',
    function drinkControllers($scope, $http) {
     
     $scope.name = "drinking Buddy";
+    $scope.inputPanel = true;
+    $scope.statusPanel = false;
+    $scope.searchPanel = false;
+    $scope.socialPanel = false;
+    
+    $scope.form = {
+        pid:"abc",
+        pname: "Peter",
+        pheight: 5,
+        pweight: 120
+    }
+    $scope.submit = function(searchForm) {
+        console.log("submit");
+        $scope.inputPanel = false;
+        $scope.statusPanel = true;
+        $scope.searchPanel = false;
+        $scope.socialPanel = false;        
+    };
+    $scope.openStatusPanel = function(){
+        console.log("status");
+        
+        $scope.inputPanel = false;
+        $scope.statusPanel = true;
+        $scope.searchPanel = false;
+        $scope.socialPanel = false;                
+    };
+    $scope.openSearchPanel = function(){
+        console.log("search");
+
+        $scope.inputPanel = false;
+        $scope.statusPanel = false;
+        $scope.searchPanel = true;
+        $scope.socialPanel = false;                
+    };
+    $scope.openSocialPanel = function(){
+        console.log("social");
+
+        $scope.inputPanel = false;
+        $scope.statusPanel = false;
+        $scope.searchPanel = false;
+        $scope.socialPanel = true;                
+    };    
 });
                             
